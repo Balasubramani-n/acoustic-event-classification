c03e2326e757152714bd04fd5a329c58d93c40a9
Balasubramani N <balasubramani01052002@gmail.com>
2023-05-06 19:06:01 +0530

    This modified module of AEC adds more dynamism

--- ensembleModel.py
+++ ensembleModel.py
@@ -1,125 +1,59 @@
-#MODULE1 : HYBRID MODEL(Model averaging)
-
-"""
-In this approach, you can average the predictions or outputs of multiple models to
-create a combined prediction or output. This can be done at inference time by
-passing the same input through multiple models and averaging their predictions,
-or during training by averaging the outputs of multiple models during forward
-pass or aggregating their gradients during backward pass.
-
-"""
-#hello world
-TRAIN = True
-
-
-import torch
-import torchaudio.transforms as T
-import torch.nn.functional as F
-from torch import nn
-from torch.utils.data import DataLoader , Dataset
-from tqdm import tqdm
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-import us8k_dataset as ds1
-import rnn_model as M1
+import coreConfig as cc
+from MyModels import *
 import MyMetrics as met
+import dataset as usd
+exec(cc.stmts)
 
-trLossList , tstLossList  , trAcc , testAcc = [] , [] , [] , [] 
-ACCURACY = [] 
-BATCH_SIZE = 32
-EPOCHS  = 10
-LR = 0.001
-ANNOTATIONS_FILE = "C:\\Users\\nagav\\OneDrive\\Documents\\UrbanSound8K.tar\\UrbanSound8K\\metadata\\set6.csv"
-AUDIO_DIR = "C:\\Users\\nagav\\OneDrive\\Documents\\UrbanSound8K.tar\\UrbanSound8K\\audio"
-SAMPLE_RATE = 22050*4 
-NUM_SAMPLES = 22050*4
+def bestFit(models , inp) :
+    interLogits = [] #intermediate logits 
+    for model , info in models.items() :
+        x = inp[info['spec']].to(device)
+        pred = model(x.view(*info['inDim']))
+        interLogits.append(pred) 
+    resultLogits = torch.stack(interLogits , dim = 0)
+    max_values, max_indices = torch.max(resultLogits, dim=0)
+    result = torch.squeeze(max_values)
+    return result
+        
 
-INP = 171            #INPUTS 
-HID = BATCH_SIZE     #NUMBER OF HIDDEN LAYERS 
-NL  = 8              #NUMBER OF LAYERS 
-NU  = 1              #NUMBER OF UNITS 
-NCLS = 10            #NUMBER OF CLASSES
-NH = 16
-
-PT_FILE = "Trained\\rnnV3.pth"
-
-# Define multiple models
-
-# Define averaging function
-def average_models(models, x):
-    outputs = [model(x.view(*v)) for model , v in models.items()]
-    combined_output = torch.mean(torch.stack(outputs), dim=0)
-    return combined_output
-
-def test_single_epoch(models , data_loader , loss_fun , device ) :
-    for k , v  in models.items():
+def test_single_epoch(models , data_loader , loss_fun ) :
+    for k in models.keys() :
         k.eval() 
     with torch.inference_mode() :
         for inp , tar in data_loader :
-            inp , tar = inp.to(device) , tar.to(device)
-            logits = average_models(models, inp)
+            logits = bestFit(models, inp)
+            tar = tar.to(torch.int64).to(device)
             loss = loss_fun(logits , tar)
-            acc = met.acc(logits , tar)
-        print(f"loss :{loss} testing accuracy {acc}")
+            acc = met.acc(logits , tar)    
+            print(f"testing accuracy {acc}")
 
-def evaluate(models , data_loader , loss_fun ,  device , epochs ):
 
-    for i in tqdm(range(epochs) , desc = "->"):
-        test_single_epoch(models ,  data_loader  , loss_fun , device)
-    print("Testing finished")
-
-# Call the averaging function during inference
-if __name__  == "__main__" :
-
+def getModels():
+    models = {}
+    for model_name , info in cc.models.items() :
+        model = globals()[model_name](*info['params']) if info['params'] else globals()[model_name]()
+        if os.path.exists(info['path']):
+            status = model.load_state_dict(torch.load(info['path']))
+            print(model_name , status)
+        model.to(device)
+        models.update({model : {"inDim" : info["inDim"] , "spec" : info["spec"]}})
+    return models  
+    
     
 
+if __name__ == "__main__" :
 
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"Device using :{device}")
-                                                           
-    transform = T.MFCC(
-        sample_rate=SAMPLE_RATE,
-        n_mfcc=64,
-        melkwargs={"n_fft": 1024, "hop_length": 512, "n_mels": 64, "center": False},
-        )
-
+    dataLoader = DataLoader(usd.UrbanSoundDataset(
+                toDB = {"mfcc" : False , "mel" : True},
+                spec = None,
+                train = True,
+                test_fold = [1]
+                ),
+                batch_size=cc.batch_size, shuffle=True , drop_last = cc.drop_last)
     
-    usd = ds1.UrbanSoundDataset(
-        ANNOTATIONS_FILE ,
-        AUDIO_DIR ,
-        transform ,
-        SAMPLE_RATE ,
-        NUM_SAMPLES ,
-        device
-        )
-    #creating a custom dataloader and splitting the data 
     
-    data_loader = DataLoader(usd , batch_size = BATCH_SIZE,shuffle = True,drop_last=True)
+    models = getModels()
+    loss_fun = nn.CrossEntropyLoss().to(device)
+    test_single_epoch(models , dataLoader , loss_fun )
     
-    PT_FILE = "Trained\\rnnV3.pth"
-    bgru1 = M1.RNN_GRU(input_size = INP, hidden_size = HID, num_layers = NL , output_size = NCLS).to(device)
-    bgru1.load_state_dict(torch.load(PT_FILE))
-
-    PT_FILE = "Trained\\rnnV3.pth"
-    bgru2 = M1.RNN_GRU(input_size = INP, hidden_size = HID, num_layers = NL , output_size = NCLS).to(device)
-    bgru2.load_state_dict(torch.load(PT_FILE))
-
-    PT_FILE = "Trained\\rnnV3.pth"
-    bgru3 = M1.RNN_GRU(input_size = INP, hidden_size = HID, num_layers = NL , output_size = NCLS).to(device)
-    bgru3.load_state_dict(torch.load(PT_FILE))
-    
-    models = { bgru1 : (-1, 64, 171) , bgru2 : (-1, 64, 171) , bgru3 : (-1, 64, 171) }
-
-    
-    if(TRAIN) :
-        loss_fun = nn.CrossEntropyLoss().to(device)
-        evaluate(models , data_loader , loss_fun ,  device , EPOCHS)
-
-        
-        
-        
-
-
-
+    print("done")
